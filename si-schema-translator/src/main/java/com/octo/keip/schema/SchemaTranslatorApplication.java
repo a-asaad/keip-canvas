2dd957f135610ed3634d9de3a9593be65a7ade39
Abdullah Asaad <abdullah.asaad@connexta.com>
2024-04-02 13:03:34 -0700

    Add cli runner

--- si-schema-translator/src/main/java/com/octo/keip/schema/SchemaTranslatorApplication.java
+++ si-schema-translator/src/main/java/com/octo/keip/schema/SchemaTranslatorApplication.java
@@ -1,20 +1,12 @@
 package com.octo.keip.schema;
 
-import com.octo.keip.schema.client.XmlSchemaHttpClient;
-import com.octo.keip.schema.config.XmlSchemaSourceConfiguration;
-import java.io.InputStream;
+import com.octo.keip.schema.cmd.TranslateCommand;
+import picocli.CommandLine;
 
 // TODO: Rename packages
 public class SchemaTranslatorApplication {
   public static void main(String[] args) {
-    XmlSchemaSourceConfiguration config = XmlSchemaSourceConfiguration.readYaml(getYaml());
-    var schemaClient = new XmlSchemaHttpClient(config.getImportedSchemaLocationsMap());
-    var translation = new EipSchemaTranslation(config, schemaClient);
-  }
-
-  private static InputStream getYaml() {
-    return SchemaTranslatorApplication.class
-        .getClassLoader()
-        .getResourceAsStream("schemaConfig.yaml");
+    int exitCode = new CommandLine(new TranslateCommand()).execute(args);
+    System.exit(exitCode);
   }
 }
